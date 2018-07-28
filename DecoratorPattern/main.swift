89911278675d931e62c196d90252f44064680c80
mohsen khodadadzadeh <mohsenkhodadadzadeh@mohsens-macbook-pro.local>
2018-07-28 20:31:22 +0430

    Initial Commit

---
+++ DecoratorPattern/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DecoratorPattern
+//
+//  Created by mohsen khodadadzadeh on 7/28/18.
+//  Copyright © 2018 mohsen khodadadzadeh. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
